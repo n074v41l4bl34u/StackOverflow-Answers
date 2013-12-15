75513a9c8ac299887a9ef62da60390ec284e94c5
Infinum <n074v41l4bl34u@gmail.com>
2013-12-15 22:10:03 +0100

    Remove some commented-out code

--- SO19796132 KeywordSearch/SearchDomain.cs
+++ SO19796132 KeywordSearch/SearchDomain.cs
@@ -9,10 +9,5 @@ namespace SO19796132KeywordSearch
     public List<Category> Categories { get; set; }
 
     public List<KeywordAdCategory> KeywordAdCategories { get; set; }
-
-    //public IEnumerable<KeywordSearch> KnownKeywordSearches()
-    //{
-    //  return Keywords.SelectMany(k=>k.Name.Split('-').)
-    //}
   }
 }
