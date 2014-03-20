41e7df781cefcd19d3770861c7111bcc70630edb
Infinum <n074v41l4bl34u@gmail.com>
2014-03-20 21:00:38 +0100

    Added answer to: 6480577 http://stackoverflow.com/questions/6480577/c-sharp-generics-how-to-compare-values-of-generic-types

---
+++ SO6480577 CompareGenerics/Bar.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Bar.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SO6480577_CompareGenerics
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+
+  /// <summary>
+  /// TODO: Update summary.
+  /// </summary>
+  public class Bar<T> where T: IComparable<T>//: IComparable<Bar<T>> where T : IComparable<T>
+  {
+    T _comparableValue;
+
+    //public int CompareTo(Bar<T> other)
+    //{
+    //  // do own logic
+    //  return _comparableValue.CompareTo(other._comparableValue);
+    //}
+
+    public bool IsInRange(T value)
+    {
+      return value.CompareTo(_comparableValue) >= 0;
+    }
+
+    //public static bool operator <(Bar<T> a, Bar<T> b)
+    //{
+    //  return a.CompareTo(b) < 0;
+    //}
+
+    //public static bool operator >(Bar<T> a, Bar<T> b)
+    //{
+    //  return a.CompareTo(b) > 0;
+    //}
+
+    //public static bool operator <=(Bar<T> a, Bar<T> b)
+    //{
+    //  return a.CompareTo(b) <= 0;
+    //}
+
+    //public static bool operator >=(Bar<T> a, Bar<T> b)
+    //{
+    //  return a.CompareTo(b) >= 0;
+    //}
+  }
+}
